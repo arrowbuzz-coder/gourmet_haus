921bc938c6c1dac763fcda349e37ffdc2618921f
arrowbuzz-coder <arrowbuzz.notifications@gmail.com>
2025-12-13 00:11:51 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,16 +41,16 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/logo_bistro.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/logo_bistro.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/logo_bistro.png",
+        type: "image/png",
       },
     ],
     apple: "/apple-icon.png",
